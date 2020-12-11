717bce7d27f43bf7dc98d48355e8bef7707842b8
TilenOkretic <tilen.okretic@gmail.com>
2020-12-11 14:36:40 +0000

    working on domaca naloga 2

--- domaca_naloga_1/main.c
+++ domaca_naloga_1/main.c
@@ -3,6 +3,36 @@
 
 #include <stdio.h>
 
+float kelvin(float temp);
+float fahrenheit(float temp);
+float konverzija(float temp, char izbor);
+
+int main()
+{
+    
+    float celzija;
+    char ch;
+
+    printf("Vnesi stopinjev stopinjah celzija: ");
+    scanf("%f", &celzija);
+    
+    printf("Vnesi tip konvevrzije  [k ... pretvorba v kelvin] [f ... pretvorba v fahrenheit]:\n");
+    scanf(" %c", &ch);
+    
+    float out = konverzija(celzija, ch);
+    
+    switch(ch){
+        case 'k':
+            printf("\nRezultat pretvorbe %f stopinj celzija v kelvine je %f kelvin\n", celzija, out);
+            break;
+        case 'f':
+            printf("\nRezultat pretvorbe %f stopinj celzija v fahrenheit je %f stopinj fahrenheit\n", celzija, out);
+            break;
+    }
+    
+    return 0;
+}
+
 float kelvin(float temp) {
     return temp + 273.15;
 }
@@ -32,30 +62,4 @@ float konverzija(float temp, char izbor) {
             printf("Nevelajven izbor\n[k ... pretvorba v kelvin]\n[f ... pretvorba v fahrenheit]\n");
             break;
     }
-}
-
-int main()
-{
-    
-    float celzija;
-    char ch;
-
-    printf("Vnesi stopinjev stopinjah celzija: ");
-    scanf("%f", &celzija);
-    
-    printf("Vnesi tip konvevrzije  [k ... pretvorba v kelvin] [f ... pretvorba v fahrenheit]:\n");
-    scanf(" %c", &ch);
-    
-    float out = konverzija(celzija, ch);
-    
-    switch(ch){
-        case 'k':
-            printf("\nRezultat pretvorbe %f stopinj celzija v kelvine je %f kelvin\n", celzija, out);
-            break;
-        case 'f':
-            printf("\nRezultat pretvorbe %f stopinj celzija v fahrenheit je %f stopinj fahrenheit\n", celzija, out);
-            break;
-    }
-    
-    return 0;
 }
